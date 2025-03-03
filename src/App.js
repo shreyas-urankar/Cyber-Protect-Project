fd508ecd6ef9ba0847a238414762d87ffa8d5c3b
shreyas-urankar <urankarshreyas@gmail.com>
2025-03-03 15:03:28 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
+import { HashRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <HeroSection />
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Navbar />
+        <HeroSection />
+      </div>
+    </HashRouter>
   );
 }
 
-
 export default App;
-
-
-
-
-
-
