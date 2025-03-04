9b264cd34759191235048aad07e3367a6a7116c7
shreyas-urankar <urankarshreyas@gmail.com>
2025-03-04 22:05:09 +0530

    .

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
