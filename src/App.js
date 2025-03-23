310b59bcd350ccebd0def3c23d044089a280721a
SHREYAS PRASHANT URANKAR <urankarshreyas@gmail.com>
2025-03-24 03:38:55 +0530

    Added to deploy on vercel

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { HashRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import RiskAssessments from "./RiskAssessments";
+import CaseStudies from "./CaseStudies"; // ✅ Import CaseStudies Component
 
-function App() {
+const App = () => {
   return (
-    <HashRouter>
-      <div className="App">
-        <Navbar />
-        <HeroSection />
-      </div>
-    </HashRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/risk-management" element={<RiskAssessments />} />
+        <Route path="/case-studies" element={<CaseStudies />} /> {/* ✅ Added Route for Case Studies */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
