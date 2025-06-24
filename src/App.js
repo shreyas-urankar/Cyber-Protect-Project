575bfb4f85191fdd6e43dcdb8eae6aef42cec3bf
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-06-24 23:59:44 +0530

    Removed advisory

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
 import RiskAssessments from "./components/RiskAssessments";
 import CaseStudies from "./components/CaseStudies";
-import Advisory from "./components/Advisory";
 import GetStarted from "./components/GetStarted";
 import AboutUs from "./components/AboutUs";
 import TecnologyConsultant from "./components/TechnologyConsultant";
@@ -13,10 +12,7 @@ import Blog from "./components/Blog";
 import ComplianceAssessment from "./components/compliance_assessment";
 import AIML from "./components/AIML";
 import IotSecurity from "./components/IotSecurity";
-
-
 import ContactPage from "./components/Contact";
-
 import CloudSecurity from "./components/CloudSecurity";
 import MobileWebSecurity from "./components/MobileWebSecurity";
 import ScrollToTop from "./components/ScrollToTop";
@@ -30,18 +26,19 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/risk-management" element={<RiskAssessments />} />
         <Route path="/case-studies" element={<CaseStudies />} />
-        <Route path="/advisory" element={<Advisory />} />
         <Route path="/getstarted" element={<GetStarted />} />
         <Route path="/technologyconsultant" element={<TecnologyConsultant />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/aiml" element={<AIML />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="/compliance_assessment" element={<ComplianceAssessment />} />
+        <Route
+          path="/compliance_assessment"
+          element={<ComplianceAssessment />}
+        />
         <Route path="/iot-security" element={<IotSecurity />} />
         <Route path="/cloudsecurity" element={<CloudSecurity />} />
         <Route path="/mobilewebsecurity" element={<MobileWebSecurity />} />
-
       </Routes>
       <Footer />
     </Router>
