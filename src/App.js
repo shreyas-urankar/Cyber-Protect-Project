39ea33ed181f9b05b6cd9f1ef5aaf7785c65654c
codeby-vikrant <workwithvikrant0111@gmail.com>
2025-06-30 00:25:31 +0530

    Removed about us paths

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import HomePage from "./components/HomePage";
 import RiskAssessments from "./components/RiskAssessments";
 import CaseStudies from "./components/CaseStudies";
 import GetStarted from "./components/GetStarted";
-import AboutUs from "./components/AboutUs";
 import TecnologyConsultant from "./components/TechnologyConsultant";
 import Blog from "./components/Blog";
 import ComplianceAssessment from "./components/compliance_assessment";
@@ -28,7 +27,6 @@ const App = () => {
         <Route path="/case-studies" element={<CaseStudies />} />
         <Route path="/getstarted" element={<GetStarted />} />
         <Route path="/technologyconsultant" element={<TecnologyConsultant />} />
-        <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/aiml" element={<AIML />} />
         <Route path="/contact" element={<ContactPage />} />
